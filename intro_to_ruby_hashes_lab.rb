39c101d13397d725ed014dd8959a8aa287a3fc8c
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-09-28 22:01:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def monopoly_with_third_tier
     :rent_in_dollars => {
       :one_piece_owned => 25,
       :two_pieces_owned => 50,
-      :key_three => {},
+      :three_pieces_owned => 100,
       :key_four => {}
     },
     :names => {
