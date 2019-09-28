5c543eb63078b4e3f1c94f8b76eea395a0d61b06
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-09-28 22:02:46 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,7 +30,7 @@ def monopoly_with_third_tier
     :names => {
       :reading_railroad => {},
       :pennsylvania_railroad => {},
-      :names_three => {},
+      :b_and_o_railroad => {},
       :names_four => {}
     }
   }
